692a39117357af0595456d890fd7b8082e9256cf
Ami <ami.m.nahid@gmail.com>
2025-07-24 10:45:43 -0400

    added a second page and made an image a navigation button to the 2nd page

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,31 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color (red: 255/255, green: 187/255, blue: 215/255)
-                .ignoresSafeArea()
-            
-            VStack {
-                Text ("ABOUT ME")
-                    .font(.title)
-                    .foregroundColor(Color(hue: 0.842, saturation: 0.698, brightness: 0.913))
-                Spacer()
-                    .frame(width: 0, height: 100)
+        NavigationStack {
+            ZStack {
+                //set background color
+                Color (red: 255/255, green: 187/255, blue: 215/255)
+                    .ignoresSafeArea()
                 
-                Image ("ami")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .clipShape(Circle())
-                
-                Text ("Ami Nahid")
-                    .font(.body)
-                    .foregroundColor(Color(hue: 0.842, saturation: 0.698, brightness: 0.913))
-                
-                HStack {
+                VStack {
+                    //title
+                    Text ("ABOUT ME")
+                        .font(.title)
+                        .foregroundColor(Color(hue: 0.842, saturation: 0.698, brightness: 0.913))
+                    Spacer()
+                        .frame(width: 0, height: 100)
                     
+                    //image of me and my name
+                    Image ("ami")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+            
+                    Text ("Ami Nahid")
+                        .font(.body)
+                        .foregroundColor(Color(hue: 0.842, saturation: 0.698, brightness: 0.913))
+                    Spacer()
+                        .frame(width: 0, height: 80)
+                    
+                    //pictures that will be buttons
+                    HStack {
+                        NavigationLink(destination: HobbiesPage()) {
+                            VStack {
+                                Image ("baby")
+                                    .resizable()
+                                    .frame(width: 120, height: 170)
+                                Text("My Hobbies")
+                            }
+                            
+                        }
+                       
+                        Spacer ()
+                            .frame (width: 40, height: 10)
+                        Image ("birthday")
+                            .resizable()
+                            .frame (width: 150, height: 200)
+                    }
+                  
                 }
             }
         }
+
         
     }
 }
---
+++ AboutMe/HobbiesPage.swift
@@ -0,0 +1,26 @@
+//
+//  HobbiesPage.swift
+//  AboutMe
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct HobbiesPage: View {
+    var body: some View {
+        ZStack {
+            //set background color
+            Color (red: 255/255, green: 187/255, blue: 215/255)
+                .ignoresSafeArea()
+            
+            Text ("Hi! I'm Ami and I'm 17 years old, and a rising senior at Wootton HS. I play tennis and love to run")
+                .font(.system(size: 16, design: .rounded))
+            
+          
+                
+        }
+        
+    }
+}
